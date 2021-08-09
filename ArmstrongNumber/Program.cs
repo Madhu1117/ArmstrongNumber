d027d2cf3ac638bb301c7eb8978999ddb93fffbe
nani.madhu1117 <nani.madhu1117@gmail.com>
2021-08-09 12:12:26 +0530

    Add project files.

---
+++ ArmstrongNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 153;
+            int r = 0;
+            int cube = 0;
+            int sum = 0;
+            int temp = n;
+            for (int i=1; i <= 3; i++)
+            {
+                r = n % 10;
+                cube = r * r * r;
+                sum = sum + cube;
+                n = n / 10;
+            }
+            if (sum == temp)
+            {
+                Console.WriteLine("Given Number is Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("Given Number is Not an Armstrong Number");
+            }
+        }
+    }
+}
